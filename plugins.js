1208f44e613170937196edfc0429b585b1af63f1
Ahmed-Abdel-Mgeed <31767300+Ahmed-Abdel-Mgeed@users.noreply.github.com>
2018-01-12 00:10:46 +0200

    onther Update

--- plugins.js
+++ plugins.js
@@ -2,19 +2,19 @@
 $(document).ready(function () {
  	// Get the form &  the submit button.
     const myForm = $("#myForm"),
-    	btn    = myForm.children().last().children(),
-    	grid   = $("#grid"); // get the table.
+        	btn    = myForm.children().last().children(),
+        	grid   = $("#grid"); // get the table.
 
 
 
 
     // listening to the submit button.
-    
-    btn.click(function (evt) {
-        evt.preventDefault(); 	// stop the page from reload.
 
-        mackGrid();				// call the makeGrid function.
-        Actions();
+    btn.click(function (evt) {
+      evt.preventDefault(); 	// stop the page from reload.
+
+      mackGrid();				// call the makeGrid function.
+      Actions();        // call the Actions function.
     });
 
 
@@ -24,7 +24,7 @@ $(document).ready(function () {
     function mackGrid() {
 
     	// clear the old canvas if the user try to make a new one.
-		grid.children().remove(); 
+		grid.children().remove();
 
     	let	gridHeight = $("#gridHeight").val(),// get the height that assigned by the user.
     		gridWidth = $("#gridWidth").val();	// get the width that assigned by the user.
@@ -41,7 +41,7 @@ $(document).ready(function () {
 
     let td = grid.children().last();
 	/*	set an event listner to the user actions with the canvas.
-		TO DO: on click color = the user color & on dbl click delete the color */
+		  TO DO: on click color = the user color & on dbl click = delete the color */
 	grid.on("click dblclick", "td", function (event) {
 		// Get the color from color paicker.
 		let color = $("#colorPaicker").val();
@@ -49,18 +49,26 @@ $(document).ready(function () {
 		event.type == "click" ? $(this).css("background", color) : $(this).css("background", "#ffffff5c");
 	});
 
+  /**
+    @description Creat a Actions function
+    that we will use to know the numper of clicks on the
+    button to toggle the text on it and control the slide Actions for boxs.
+  **/
     let x = 0;
 
   function Actions() {
-    $( ".num-group" ).toggle( "slide" );
-    $( ".color" ).toggle( "slide", { direction: "right" });
-    
-    x += 1;
-    if (x % 2 == 0){
-      btn.text("Let's Drawing");
+    $( ".num-group" ).toggle("slide");  // to slide the div.num-groub to the left.
+    $( ".color" ).toggle( "slide", { direction: "right" }); // to slide the div.color to the left.
+
+    x += 1; // increase the x variable by one every time the user clicks the button.
+     if (x % 2 != 0){
+      btn.text("Start Over");  // that will be the value of button if the user clicks the button.
+      $(".grid ul").toggle("slide");
     }
-    else if (x % 2 != 0){
-      btn.text("Start Over");
+
+    else if (x % 2 == 0){
+      btn.text("Let's Drawing");  // that will be the value of button if the user clicks the button again.
+      $(".grid ul").toggle("slide");  // that will slide in when the user click the button and slide out if the user click start Over button.
     }
   };
 });
